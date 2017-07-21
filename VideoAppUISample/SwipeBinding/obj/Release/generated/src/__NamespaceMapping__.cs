bce4ea954a0b959420c09f686b6ee039aa463264
waqas munawar <waqas@faizans-Mac-mini.local>
2017-07-21 12:20:28 +0500

    Remove ignored files

--- VideoAppUISample/SwipeBinding/obj/Release/generated/src/__NamespaceMapping__.cs
+++
@@ -1,4 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.daimajia.swipe", Managed="Com.Daimajia.Swipe")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.daimajia.swipe.adapters", Managed="Com.Daimajia.Swipe.Adapters")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.daimajia.swipe.interfaces", Managed="Com.Daimajia.Swipe.Interfaces")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.daimajia.swipe.util", Managed="Com.Daimajia.Swipe.Util")]
